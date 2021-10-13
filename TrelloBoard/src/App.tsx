7f1a406157827355350970a78fe97787bde3c707
Thuan <ttctde@gmail.com>
2021-10-13 22:13:29 +0700

    unit commit

---
+++ TrelloBoard/src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='header row'>
+        <ul className='header__column--1'>
+          <li>text content</li>
+        </ul>
+        <ul className='header__column--1'>
+          <li>text content</li>
+        </ul>
+        <ul className='header__column--1'>
+          <li>text content</li>
+        </ul>
+        <ul className='header__column--1'>
+          <li>text content</li>
+        </ul>
+        <ul className='header__column--1'>
+          <li>text content</li>
+        </ul>
+        <ul className='header__column--1'>
+          <li>text content</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ TrelloBoard/src/index.tsx
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import './assets/style/sass/main.scss';
+ReactDOM.render(<App />, document.getElementById('root'));
