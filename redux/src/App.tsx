62e2eafed04250e63d5876ccd94bdfc8fcf139d5
thuan <ttctde@gmail.com>
2021-10-26 13:47:40 +0700

    update

--- redux/src/App.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-const App:React.FC = ()=>{
-  
-}
-export default App
--- redux/src2/redux/store.js
+++
@@ -1,5 +0,0 @@
-import { createStore } from "react";
-import { counterReducer } from "./reducer";
-export const store=createStore(
-    counterReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({trace:true}));
