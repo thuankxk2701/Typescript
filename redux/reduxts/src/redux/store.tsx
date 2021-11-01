c07dd4059fd444cf2939de98109eb78d02e9b3a8
thuan <ttctde@gmail.com>
2021-11-01 13:16:30 +0700

    init commit

--- redux/reduxts/src/redux/store.tsx
+++ redux/reduxts/src/redux/store.tsx
@@ -1,5 +1,11 @@
 import createStore from './server/createStore';
 import { counterReducer } from './reducer';
+interface IsWindow extends Window {
+  __REDUX_DEVTOOLS_EXTENSION__: any;
+}
+declare const window: IsWindow;
 
-
-export const store = createStore(counterReducer,)
+export const store = createStore(
+  counterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
