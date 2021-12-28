c4899e4ed7fa84e735efdbffad0f8fe51f99f783
Thuan <ttctde@gmail.com>
2021-12-28 13:26:17 +0700

    update category own header article

--- react-readux-shoppe-card/client/src/components/home/HomeArticle/HomeArticle.tsx
+++ react-readux-shoppe-card/client/src/components/home/HomeArticle/HomeArticle.tsx
@@ -1,13 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import "./HomeArticle.scss";
+import { Link } from "react-router-dom";
+import { numberListFrames } from "../../../redux/types";
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
+import classNames from "classnames";
 
 const HomeArticle: React.FC = () => {
+  const [isArrowCarousel, setIsArrowCarousel] = useState<Boolean>(true);
+  const handleToggleCarousel = () => {
+    setIsArrowCarousel(!isArrowCarousel);
+  };
+
   return (
     <div className="home__article">
       <div className="home__article--banner-image">
         <img src="https://cf.shopee.vn/file/b4b3ae7cd45ce23a678d172112357793" alt="" />
       </div>
-      <div className="category"></div>
+      <div className="home__article--category">
+        <div className="home__article--category-title">
+          <h2>Danh mục</h2>
+        </div>
+        <button
+          onClick={handleToggleCarousel}
+          className={classNames("home__article--category-arrow_left", { active: !isArrowCarousel })}
+        >
+          <BsChevronLeft />
+        </button>
+        <div className="home__article--category-box">
+          <ul className="home__article--category-carousel">
+            {numberListFrames.map((lists: any, index: number) => (
+              <li key={index} className="home__article--category-carousel_item">
+                <Link
+                  to="/"
+                  className={classNames("home__article--category-carousel_item__frames", {
+                    active: !isArrowCarousel,
+                  })}
+                >
+                  <div className={lists.classImageTop}></div>
+                  <span>{lists.textTop}</span>
+                </Link>
+                <Link
+                  to="/"
+                  className={classNames("home__article--category-carousel_item__frames", {
+                    active: !isArrowCarousel,
+                  })}
+                >
+                  <div className={lists.classImageBottom}></div>
+                  <span>{lists.textBottom}</span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <button
+          onClick={handleToggleCarousel}
+          className={classNames("home__article--category-arrow_right", { active: isArrowCarousel })}
+        >
+          <BsChevronRight />
+        </button>
+      </div>
+      <div>text</div>
     </div>
   );
 };
---
+++ react-readux-shoppe-card/client/src/redux/types.tsx
@@ -0,0 +1,80 @@
+export const numberListFrames = [
+  {
+    classImageTop: "home__article--category-carousel_item__frames-image--top-1",
+    textTop: "Thời Trang Nam",
+    classImageBottom: "home__article--category-carousel_item__frames-image--bottom-1",
+    textBottom: "Thời Trang Nữ",
+  },
+  {
+    classImageTop: "home__article--category-carousel_item__frames-image--top-2",
+    textTop: "Điện Thoại & Phụ Kiện",
+    classImageBottom: "home__article--category-carousel_item__frames-image--bottom-2",
+    textBottom: "Mẹ & Bé",
+  },
+  {
+    classImageTop: "home__article--category-carousel_item__frames-image--top-3",
+    textTop: "Thiết Bị Điện Tử ",
+    classImageBottom: "home__article--category-carousel_item__frames-image--bottom-3",
+    textBottom: "Nhà Cửa & Đời Sống",
+  },
+  {
+    classImageTop: "home__article--category-carousel_item__frames-image--top-4",
+    textTop: "Máy Tính & Láp Tốp",
+    classImageBottom: "home__article--category-carousel_item__frames-image--bottom-4",
+    textBottom: "Sắc Đẹp",
+  },
+  {
+    classImageTop: "home__article--category-carousel_item__frames-image--top-5",
+    textTop: "Máy ảnh & Máy Quay Phim",
+    classImageBottom: "home__article--category-carousel_item__frames-image--bottom-5",
+    textBottom: "Sức Khỏe",
+  },
+  {
+    classImageTop: "home__article--category-carousel_item__frames-image--top-6",
+    textTop: "Đòng Hồ",
+    classImageBottom: "home__article--category-carousel_item__frames-image--bottom-6",
+    textBottom: "Giày Dép Nữ",
+  },
+  {
+    classImageTop: "home__article--category-carousel_item__frames-image--top-7",
+    textTop: "Giày Dép Nam",
+    classImageBottom: "home__article--category-carousel_item__frames-image--bottom-7",
+    textBottom: "Túi Ví Nữ",
+  },
+  {
+    classImageTop: "home__article--category-carousel_item__frames-image--top-8",
+    textTop: "Thiết Bị Điện Gia Dụng",
+    classImageBottom: "home__article--category-carousel_item__frames-image--bottom-8",
+    textBottom: "Phụ Kiện & Trang Sức Nữ",
+  },
+  {
+    classImageTop: "home__article--category-carousel_item__frames-image--top-9",
+    textTop: "Thể Thao & Du Lịch",
+    classImageBottom: "home__article--category-carousel_item__frames-image--bottom-9",
+    textBottom: "Bách Hóa Online",
+  },
+  {
+    classImageTop: "home__article--category-carousel_item__frames-image--top-10",
+    textTop: "Ô Tô & Xe Máy & Xe Đạp",
+    classImageBottom: "home__article--category-carousel_item__frames-image--bottom-10",
+    textBottom: "Nhà Sách Online",
+  },
+  {
+    classImageTop: "home__article--category-carousel_item__frames-image--top-11",
+    textTop: "Balo & Túi Ví Nam",
+    classImageBottom: "home__article--category-carousel_item__frames-image--bottom-11",
+    textBottom: "Thời Trang Trẻ Em",
+  },
+  {
+    classImageTop: "home__article--category-carousel_item__frames-image--top-12",
+    textTop: "Đồ Chơi",
+    classImageBottom: "home__article--category-carousel_item__frames-image--bottom-12",
+    textBottom: "Giặt Giũ & Chăm Sóc Nhà Cửa",
+  },
+  {
+    classImageTop: "home__article--category-carousel_item__frames-image--top-13",
+    textTop: "Chăm Sóc Thú Cưng",
+    classImageBottom: "home__article--category-carousel_item__frames-image--bottom-13",
+    textBottom: "Voucher & Dịch Vụ",
+  },
+];
