a2fe9bb102a852db3acd8307c4b1dbc3890247be
Thuan <ttctde@gmail.com>
2022-01-15 13:46:24 +0700

    Done Count

--- redux/reduxts/src/components/Counter.tsx
+++ redux/reduxts/src/components/Counter.tsx
@@ -4,16 +4,17 @@ import {CounterStateProps} from '../redux/reducer'
 const  Counter=()=> {
     const count=useSelector<CounterStateProps>((state)=>state.counter)
    const dispatch=useDispatch(); 
-   
-    return (
-        <div>
-            Counter Value:{count}
-            <br/>
-            <button onClick={()=>dispatch({type:'increment'})}>+</button>
-            <button onClick={()=> dispatch({type:'decrement'})}>-</button>
-            
-        </div>
-    )
+   const handleChangeSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+     e.preventDefault();
+   };
+   return (
+     <form onSubmit={handleChangeSubmit}>
+       Counter Value:{count}
+       <br />
+       <button onClick={() => dispatch({ type: "increment" })}>+</button>
+       <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+     </form>
+   );
 }
 
 export default Counter;
